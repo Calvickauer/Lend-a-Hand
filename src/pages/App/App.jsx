5495db48ca4bb25eaeda4ec379224eae663f3d72
calvickauer <calvickauer@gmail.com>
2023-02-09 14:54:20 -0800

    add component folder and file

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
+import Homepage from '../../components/Homepage/homepage';
 import './App.css';
 
 function App() {
