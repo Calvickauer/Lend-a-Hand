6889827d9c8772b308e531cdf508b6fdc5757d0b
gititgirl <jordanlind2017@gmail.com>
2023-02-09 18:49:01 -0800

    Search bar, add item, jordan signing off for a little

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,14 +11,19 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
+    <nav className="navBar">
+      <Link to="/search">Search</Link>
       &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
+      <Link to="/search/product">Product</Link>
+      &nbsp; | &nbsp;
+      <Link to="/add">Add</Link>
       &nbsp; | &nbsp;
       <span>Welcome, {user.name}</span>
       &nbsp; | &nbsp;
+      <Link to="/user">Profile</Link>
+      &nbsp; | &nbsp;
       <Link to="" onClick={handleLogOut}>Log Out</Link>
     </nav>
+    
   );
 }
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const profile = () => {
+  return (
+    <div>
+      <h1>profile</h1>
+      <h3>profile</h3>
+    </div>
+  )
+}
+
+export default profile
