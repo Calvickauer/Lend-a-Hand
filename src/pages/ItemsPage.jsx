acf3006f2a22829176f726a84db84667c920c7e3
gititgirl <jordanlind2017@gmail.com>
2023-02-10 11:16:25 -0800

    calendar component

--- src/pages/ItemsPage.jsx
+++ src/pages/ItemsPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import MyCalendar from '../components/Calendar/Calendar'
 import * as itemsAPI from '../utilities/item-api'
 
 export default function ItemsPage() {
@@ -18,7 +19,7 @@ export default function ItemsPage() {
             {item.map((item, idx) => 
             <div>{item.title} {item.img}</div>
             )}
-        
+            <MyCalendar />
         </>
 
     )
