7d2511518acd15a47b7496a63a5e4f5c8bce1df9
calvickauer <calvickauer@gmail.com>
2023-02-10 02:37:17 -0800

    profile layout..

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const profile = () => {
   return (
-    <div className='profile__container'>
-      <h1>profile</h1>
-      <h3>name</h3>
+    <div className='profile__page'>
+       <h1>Profile Page</h1>
+          <div className='profile__container'>
+            <div className='profile__pic'></div>
+            <div className='profile__content'>
+            <h3>"name"</h3>
+            <h4>ratings</h4>
+            <h4>reviews</h4>
+            <h4>about....</h4>
+          </div>
+      </div>
     </div>
   )
 }
