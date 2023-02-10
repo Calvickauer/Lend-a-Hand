e2028e5559e7fea03fb65a8cc7f2412408be2924
calvickauer <calvickauer@gmail.com>
2023-02-10 00:06:24 -0800

    homepage form

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 
 const homepage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-      <div className='form__container'>
+    <div className='form__container'>
         <form>
           <input className='item__input' type='text' id='item-input' placeholder='Search for items to borrow...'/>
-          <button className='search__btn' type="submit" placeholder='Search'/>
+          <input className='search__btn' type="submit" placeholder='Search'/>
         </form>
-      </div>
     </div>
   )
 }
