1b859b1c87d511b05dbf4f060d0c1c9dade550d9
gititgirl <jordanlind2017@gmail.com>
2023-02-10 10:14:02 -0800

    some styling and seed model of items

--- models/item.js
+++ models/item.js
@@ -8,6 +8,8 @@ const itemSchema = new Schema({
         required: true
     },
     title: { type: String },
+    description: { type: String },
+    image: { type: String, default: "https://wellesleysocietyofartists.org/wp-content/uploads/2015/11/image-not-found.jpg" }
 });
 
 module.exports = mongoose.model('Item', itemSchema);
